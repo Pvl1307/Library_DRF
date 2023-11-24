c2266d3b8e4c9d26c69d86fd846f872c9ef08a5f
Pvl1307 <ivanov.pavel.1307@gmail.com>
2023-11-24 13:40:46 +0300

    Book generics

--- library/views.py
+++ library/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from library.models import Book
+from library.serializers import BookSerializer
+
+
+class BookCreateAPIView(generics.CreateAPIView):
+    """Создание книги"""
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        """Сохранение созданной книги"""
+        serializer.save()
+
+
+class BookListAPIView(generics.ListAPIView):
+    """Список книг"""
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class BookRetrieveAPIView(generics.RetrieveAPIView):
+    """Просмотр выбранной книги"""
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+
+class BookUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """Обновление книги"""
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def perform_update(self, serializer):
+        """Сохранение обновления книги"""
+        serializer.save()
+
+
+class BookDestroyAPIView(generics.DestroyAPIView):
+    """Удаление выбранной книги"""
+    queryset = Book.objects.all()
